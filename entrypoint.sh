3661fbbe988e9d86e6a00153c50f6c3f45498266
Mark Tomcza <marktomcza@Mark-Tomcza-Mac.lan>
2021-08-23 12:15:39 -0500

    removed contrast-application-name and contrast-server-name

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,11 +15,9 @@ CONTRAST_API_URL=${12}
 CONTRAST_API_USERNAME=${13}
 CONTRAST_API_API_KEY=${14}
 CONTRAST_API_SERVICE_KEY=${15}
-CONTRAST_APPLICATION_NAME=${16}
-CONTRAST_AGENT_JAVA_STANDALONE_APP_NAME=${17}
-CONTRAST_APPLICATION_VERSION=${18}
-CONTRAST_SERVER_NAME=${19}
-AZURE_APPLICATION_JVM_OPTIONS=${20}
+CONTRAST_AGENT_JAVA_STANDALONE_APP_NAME=${16}
+CONTRAST_APPLICATION_VERSION=${17}
+AZURE_APPLICATION_JVM_OPTIONS=${18}
 
 cd /docker-action
 echo "creating docker image with the following inputs..."
@@ -39,12 +37,10 @@ echo "contrast-api-url: $CONTRAST_API_URL"
 echo "contrast-api-username: $CONTRAST_API_USERNAME"
 echo "contrast-api-api-key: $CONTRAST_API_API_KEY"
 echo "contrast-api-service-key: $CONTRAST_API_SERVICE_KEY"
-echo "contrast-application-name: $CONTRAST_APPLICATION_NAME"
 echo "contrast-agent-java-standalone-app-name: $CONTRAST_AGENT_JAVA_STANDALONE_APP_NAME"
 echo "contrast-application-version: $CONTRAST_APPLICATION_VERSION"
-echo "contrast-server-name: $CONTRAST_SERVER_NAME"
 echo "azure-application-jvm-options: $AZURE_APPLICATION_JVM_OPTIONS"
 
 # here we can make the construction of the image as customizable as we need
 # and if we need parameterizable values it is a matter of sending them as inputs
-docker build -t docker-action --build-arg azure_application_name="$AZURE_APPLICATION_NAME" --build-arg azure_spring_cloud_service_name="$AZURE_SPRING_CLOUD_SERVICE_NAME" --build-arg azure_file_upload_artifact_location="$AZURE_FILE_UPLOAD_ARTIFACT_LOCATION" --build-arg azure_application_artifact_location="$AZURE_APPLICATION_ARTIFACT_LOCATION" --build-arg azure_application_id="$AZURE_APPLICATION_ID" --build-arg azure_tenant_id="$AZURE_TENANT_ID" --build-arg azure_client_secret="$AZURE_CLIENT_SECRET" --build-arg azure_subscription_id="$AZURE_SUBSCRIPTION_ID" --build-arg azure_resource_group_name="$AZURE_RESOURCE_GROUP_NAME" --build-arg azure_region="$AZURE_REGION" --build-arg azure_contrast_agent_download_url="$AZURE_CONTRAST_AGENT_DOWNLOAD_URL" --build-arg contrast_api_url="$CONTRAST_API_URL" --build-arg contrast_api_username="$CONTRAST_API_USERNAME" --build-arg contrast_api_api_key="$CONTRAST_API_API_KEY" --build-arg contrast_api_service_key="$CONTRAST_API_SERVICE_KEY" --build-arg contrast_application_name="$CONTRAST_APPLICATION_NAME" --build-arg contrast_agent_java_standalone_app_name="$CONTRAST_AGENT_JAVA_STANDALONE_APP_NAME" --build-arg contrast_application_version="$CONTRAST_APPLICATION_VERSION" --build-arg contrast_server_name="$CONTRAST_SERVER_NAME" --build-arg azure_application_jvm_options="$AZURE_APPLICATION_JVM_OPTIONS" . && docker run docker-action
+docker build -t docker-action --build-arg azure_application_name="$AZURE_APPLICATION_NAME" --build-arg azure_spring_cloud_service_name="$AZURE_SPRING_CLOUD_SERVICE_NAME" --build-arg azure_file_upload_artifact_location="$AZURE_FILE_UPLOAD_ARTIFACT_LOCATION" --build-arg azure_application_artifact_location="$AZURE_APPLICATION_ARTIFACT_LOCATION" --build-arg azure_application_id="$AZURE_APPLICATION_ID" --build-arg azure_tenant_id="$AZURE_TENANT_ID" --build-arg azure_client_secret="$AZURE_CLIENT_SECRET" --build-arg azure_subscription_id="$AZURE_SUBSCRIPTION_ID" --build-arg azure_resource_group_name="$AZURE_RESOURCE_GROUP_NAME" --build-arg azure_region="$AZURE_REGION" --build-arg azure_contrast_agent_download_url="$AZURE_CONTRAST_AGENT_DOWNLOAD_URL" --build-arg contrast_api_url="$CONTRAST_API_URL" --build-arg contrast_api_username="$CONTRAST_API_USERNAME" --build-arg contrast_api_api_key="$CONTRAST_API_API_KEY" --build-arg contrast_api_service_key="$CONTRAST_API_SERVICE_KEY" --build-arg contrast_agent_java_standalone_app_name="$CONTRAST_AGENT_JAVA_STANDALONE_APP_NAME" --build-arg contrast_application_version="$CONTRAST_APPLICATION_VERSION" --build-arg azure_application_jvm_options="$AZURE_APPLICATION_JVM_OPTIONS" . && docker run docker-action
