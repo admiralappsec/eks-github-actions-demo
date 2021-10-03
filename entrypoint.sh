3e43827ec6773e70884d2db0f6e1703e93a8464a
Mark Tomcza <marktomcza@Mark-Tomcza-Mac.lan>
2021-10-03 14:51:39 -0500

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,8 +24,17 @@ cp /github/workspace/${APPLICATION_DOCKERFILE} /usr/bin/docker-action/applicatio
 echo "entering docker-action directory..."
 cp /github/workspace/${APPLICATION_MANIFESTS} /usr/bin/docker-action/application-manifests/deployment.yaml
 cd /usr/bin/docker-action
-# echo "what is inside..."
-# ls -l
+echo "what is inside - docker-action..."
+ls -l
+echo "what is inside - application-dockerfile..."
+cd /usr/bin/docker-action/application-dockerfile/
+ls -l
+echo "what is inside - application-manifests..."
+cd /usr/bin/docker-action/application-manifests/
+ls -l
+
+echo "go into build directory..."
+cd /usr/bin/docker-action
 
 #echo "creating docker image with the following inputs..."
 #echo "--------------------------------------------------"
@@ -39,4 +48,4 @@ echo "running docker build with passed arguments..."
 
 # here we can make the construction of the image as customizable as we need
 # and if we need parameterizable values it is a matter of sending them as inputs
-docker build -t docker-action --build-arg contrast_security_credentials_file="$CONTRAST_SECURITY_CREDENTIALS_FILE" --build-arg azure_credentials_file="$AZURE_CREDENTIALS_FILE" --build-arg application_manifests="/usr/bin/docker-action/application-manifests/deployment.yaml" --build-arg application_dockerfile="/usr/bin/docker-action/application-dockerfile/Dockerfile" --build-arg application_output_image_name_tag="$APPLICATION_OUTPUT_IMAGE_NAME_TAG" --build-arg cluster_name="$CLUSTER_NAME". && docker run docker-action
+docker build -t docker-action --build-arg contrast_security_credentials_file="$CONTRAST_SECURITY_CREDENTIALS_FILE" --build-arg azure_credentials_file="$AZURE_CREDENTIALS_FILE" --build-arg application_manifests="/usr/bin/docker-action/application-manifests/deployment.yaml" --build-arg application_dockerfile="/usr/bin/docker-action/application-dockerfile/Dockerfile" --build-arg application_output_image_name_tag="$APPLICATION_OUTPUT_IMAGE_NAME_TAG" --build-arg cluster_name="$CLUSTER_NAME" . && docker run docker-action
